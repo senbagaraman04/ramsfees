7b50cc38f658f45a46e7bba3312a16ef14a3e68a
Senbagaraman04 <senbagaraman04@gmail.com>
2024-03-12 16:35:55 +0530

    fix: java backend init

---
+++ backend/src/main/java/com/raman/fee/ramsfee/RamsfeeApplication.java
@@ -0,0 +1,13 @@
+package com.raman.fee.ramsfee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamsfeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RamsfeeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/raman/fee/ramsfee/RamsfeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raman.fee.ramsfee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RamsfeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
